dcfa5eaeefe21c02fbf1500d5241ba3944da97a0
sakshi mishra <sakshimishrakps@gmail.com>
2018-09-26 13:19:29 +0530

    Table Rows Updated

--- src/gui/TableBuilder.java
+++ src/gui/TableBuilder.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.util.Stack;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -9,24 +10,36 @@ import javax.swing.table.DefaultTableModel;
 
 import parser.Method;
 import parser.Parser;
+import parser.Threads;
 public class TableBuilder {
 	//To create Table in the JFrame
 	DefaultTableModel model = new DefaultTableModel();
 	private JTable table = new JTable(model);
 	private int serialNo=1;
+	
 	@SuppressWarnings("serial")
 	public TableBuilder(Parser p){
 	  model.addColumn("SNo");					//Adding columns to the JTable
 	  model.addColumn("Runtime");
 	  model.addColumn("Method");
+	  model.addColumn("Class Name");
+	  model.addColumn("Static Or Not");
+	  model.addColumn("Start Time");
+	  model.addColumn("End Time");
+	  model.addColumn("Parameter");
+	  model.addColumn("Return Type");
+	  model.addColumn("Stack Trace");
+	 
 	  for(Method met: p.getIncompleteMethods()){
-		  model.addRow(new Object[] {serialNo, met.getRuntime(),met.getMethodName()}); 
+		  model.addRow(new Object[] {serialNo, met.getRuntime(),met.getMethodName(), met.getClass(), met.isStaticOrNot(), met.getStartTime(), met.getEndTime(), met.getParameters(), met.getReturnType(), met.getMethodStack()}); 
 		  serialNo++;
 	  }
 	  for(Method met: p.sortByRuntime()){
-		  model.addRow(new Object[] {serialNo, met.getRuntime(),met.getMethodName()}); 
+		  model.addRow(new Object[] {serialNo, met.getRuntime(),met.getMethodName(), met.getClass(), met.isStaticOrNot(), met.getStartTime(), met.getEndTime(), met.getParameters(), met.getReturnType(), met.getMethodStack()}); 
 		  serialNo++;
 	  }
+	 
+	
 	  table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer(){
 		  @Override
           public Component getTableCellRendererComponent(JTable table,
